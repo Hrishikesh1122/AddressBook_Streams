24abca87cf4f570737dbccf608218c3217173bd8
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-24 11:45:28 +0530

    Added: UC1 ability to create contact

--- AddressBook/src/service/AddressBookRunner.java
+++ AddressBook/src/service/AddressBookRunner.java
@@ -1,6 +1,15 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import model.Person;
+
 public class AddressBookRunner {
+	
+	 private static Scanner sc = new Scanner(System.in);
+	 List<Person> personList = new ArrayList<>();
 
 	/**
 	 *Displays welcome message 
@@ -9,9 +18,36 @@ public class AddressBookRunner {
 		System.out.println("Welcome to address book");
 	}
 	
+	
+	
 	public static void main(String[] args) {
 		AddressBookRunner runner = new AddressBookRunner();
 		runner.displayWelcome();
+		
+		boolean isExit = false;
+		while (!isExit) {
+			System.out.println("Enter options\n1.Add\n2.Edit\n3.Delete\n4.Show\n5.Exit");
+			int userInput =sc.nextInt();
+			switch (userInput) {
+			case 1: 
+				runner.add();
+				break;
+			case 2:
+				runner.edit();
+				break;
+			case 3:
+				runner.delete();
+				break;
+			case 4:
+				runner.show();
+				break;
+			case 5 :
+				isExit=true;
+				break;
+			default :
+				System.out.println("Invalid input");
+			}
+		}
 	}
 
 }
